65c4b5eb388a5726218697bfcf2ce163660deab2
Alexander Danilin <dan-sasha@mail.ru>
2018-12-18 00:20:33 +0100

    Computation of average max height implemented

--- main.py
+++ main.py
@@ -10,9 +10,17 @@ catalan_numbers = [1, 1, 2, 5, 14, 42, 132, 429, 1430, 4862, 16796, 58786, 20801
                    1289904147324, 4861946401452]
 
 max_n = len(catalan_numbers) - 1
+
+# Fill list with -1 for each possible size of tree
+average_max_height = [-1 for i in range(max_n + 1)]
+# Set average max height for basic trees: empty and size of 1
+average_max_height[0] = 0
+average_max_height[1] = 0
+
 cur_value = 0
 
 
+# Generates uniformly(by shape) binary tree of size n
 def generate_binary_tree(n):
     global cur_value
     # print(str(cur_value) + "Current tree size" + str(n))
@@ -29,7 +37,7 @@ def generate_binary_tree(n):
     # The left subtree is size of i, the right subtree is size of n-1-i
     chance = [catalan_numbers[0] * catalan_numbers[n - 1]]
     for i in range(1, n):
-        chance.append(chance[i - 1] + catalan_numbers[0] * catalan_numbers[n - 1 - i])
+        chance.append(chance[i - 1] + catalan_numbers[i] * catalan_numbers[n - 1 - i])
 
     # Maximum value from generated array
     # print("chance" + str(n - 1))
@@ -50,15 +58,37 @@ def generate_binary_tree(n):
     return root
 
 
-tree_root = generate_binary_tree(5)
+# Computes average max height for tree of size n
+def compute_average_max_height(n):
+    if average_max_height[n] != -1:
+        return average_max_height[n]
 
-queue = [tree_root]
-while len(queue) != 0:
-    cur = queue.pop(0)
-    print(str(cur.value))
-    if cur.left_child is not None:
-        print(str(cur.value) + " is parent of left child " + str(cur.left_child.value))
-        queue.append(cur.left_child)
-    if cur.right_child is not None:
-        print(str(cur.value) + " is parent of right child " + str(cur.right_child.value))
-        queue.append(cur.right_child)
+    sum = 0
+    coef_sum = 0
+    for i in range(n):
+        coef = catalan_numbers[i] * catalan_numbers[n - 1 - i]
+        coef_sum += coef
+        sum += coef * max(compute_average_max_height(i), compute_average_max_height(n - 1 - i))
+
+    ans = 1 + sum / coef_sum
+    average_max_height[n] = ans
+
+    return ans
+
+
+if __name__ == "__main__":
+    tree_root = generate_binary_tree(5)
+
+    for i in range(max_n):
+        print("Tree of size " + str(i) + " has average maximum height equal to " + str(compute_average_max_height(i)))
+
+    # queue = [tree_root]
+    # while len(queue) != 0:
+    #     cur = queue.pop(0)
+    #     print(str(cur.value))
+    #     if cur.left_child is not None:
+    #         print(str(cur.value) + " is parent of left child " + str(cur.left_child.value))
+    #         queue.append(cur.left_child)
+    #     if cur.right_child is not None:
+    #         print(str(cur.value) + " is parent of right child " + str(cur.right_child.value))
+    #         queue.append(cur.right_child)
